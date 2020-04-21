1d2b24e6653ec2d21ce5ebec6fe0eb0bad240347
Insaf <Safinsaft@gmail.com>
2020-04-21 23:24:54 +0300

    have problems with saving fitness

--- IAI.py
+++ IAI.py
@@ -3,9 +3,10 @@ import numpy as np
 from random import randint
 from time import sleep
 
-SQUARE_SIZE = 16
-POPULATION_SIZE = 50
-CHANGE_RANGE = 0
+SQUARE_SIZE = 512
+POPULATION_SIZE = 5
+CHANGE_RANGE = 100
+GENERATION_DEPTH = 1500
 
 image_count = 0
 image_list = ["images/root/cat.jpg", "images/root/harry.jpg",
@@ -39,43 +40,40 @@ def main():
 def run():
   for start_h in range(0, H, SQUARE_SIZE):
     for start_w in range(0, W, SQUARE_SIZE):
-      # print(len(solution[start_h:start_h+SQUARE_SIZE, start_w:start_w+SQUARE_SIZE][0]))
-      # sleep(10000)
+      print(start_h, start_w)
       x = genetic( solution[start_h:start_h+SQUARE_SIZE, start_w:start_w+SQUARE_SIZE],
                reference[start_h:start_h+SQUARE_SIZE, start_w:start_w+SQUARE_SIZE])
       solution[start_h:start_h+SQUARE_SIZE, start_w:start_w+SQUARE_SIZE] = x
-      Image.fromarray(solution).show()
+      Image.fromarray(np.uint8(solution)).save("images/tries/try_0/1.jpg")
 
 def genetic(array1, array2):
   population = []
   for i in range(POPULATION_SIZE):
-    current_cost = cost(array1, array2)
-    population.append([current_cost, array1.copy()])
-  for i in range(1000):
+    current_fitness = fitness(array1, array2)
+    population.append([current_fitness, array1.copy()])
+  for i in range(GENERATION_DEPTH):
     for j in range(POPULATION_SIZE):
       population.append(mutation(population[j], array2))
-    print(len(population))
     population = selection(population)
-    print(len(population))
-  return population[0]
+    print("sorted 1", [x[0] for x in population])
+    print("sorted 2", [fitness(x[1], array2) for x in population])
+
+    Image.fromarray(np.uint8(population[0][1])).save("images/tries/try_0/1.jpg")
+
+  #print(population[0][1])
+  print(population[0][0])
+  return population[0][1]
 
 def selection(population):
   population_sorted = population.copy()
   for i in range(len(population_sorted)):
     for j in range(i+1, len(population_sorted)):
-      if population_sorted[i] > population_sorted[j]:
+      if population_sorted[i][0] < population_sorted[j][0]:
+        temp = population_sorted[i].copy()
+        population_sorted[i] = population_sorted[j].copy()
+        population_sorted[j] = temp
 
-
-  while len(population) > POPULATION_SIZE:
-    a = population.pop()
-    print(a[0])
-  return population
-
-def crossover(chromosome1, chromosome2):
-  cut = randint(1,SQUARE_SIZE-1)
-  answer1 = chromosome1[:cut] + chromosome2[cut:]
-  answer2 = chromosome2[:cut] + chromosome1[cut:]
-  return [answer1, answer2]
+  return population_sorted[0:POPULATION_SIZE]
 
 def mutation(chromosome, ref):
   answer = chromosome.copy()[1]
@@ -86,7 +84,14 @@ def mutation(chromosome, ref):
         max(min(answer[i][j][1] + randint(-CHANGE_RANGE, CHANGE_RANGE),255), 0),
         max(min(answer[i][j][2] + randint(-CHANGE_RANGE, CHANGE_RANGE),255), 0)
       ])
-  return [cost(answer, ref), answer]
+  return [fitness(answer, ref), answer]
+
+
+def crossover(chromosome1, chromosome2):
+  cut = randint(1,SQUARE_SIZE-1)
+  answer1 = chromosome1[:cut] + chromosome2[cut:]
+  answer2 = chromosome2[:cut] + chromosome1[cut:]
+  return [answer1, answer2]
 
 
 def choose_image():
@@ -97,14 +102,21 @@ def choose_image():
   H, W = image.size  # height and length of image
 
   reference = data.copy()
-  solution = np.zeros((H, W, 3))
+  solution = np.zeros((H, W, 3), dtype = "int")
 
-def cost(first, second):
+def fitness(first, second):
   sum = 0
   for i in range(len(first)):
     for j in range(len(first[0])):
-      sum += (first[i] - second[i])**2
-  return sum
+      sum += (first[i][j][0] - second[i][j][0]) ** 2
+      sum += (first[i][j][1] - second[i][j][1]) ** 2
+      sum += (first[i][j][2] - second[i][j][2]) ** 2
+  print(sum)
+  return 1e9/sum
 
 
 main()
+
+# a = [ [4,1] , [5, 2] , [4,5] , [5, 8] , [10, 3] , [50 ,10]]
+# print(selection(a))
+
